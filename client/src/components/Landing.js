b4d2dcd4b66a4104d02620ecc275db12fccf1ae7
Matthew Jaikaran <mattjaikaran@gmail.com>
2018-03-09 19:25:55 -0500

    Added a view surveys button on Dashboard

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Emaily</h1>
       <p>Collect feedback from your users</p>
+      <Link to="/surveys" className="btn-flat green text-white" style={{ color: 'white'}}>
+        View My Surveys
+      </Link>
     </div>
   )
 }
