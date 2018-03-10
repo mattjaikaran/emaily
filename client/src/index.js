afc14bb4d08685abeb38762f36257406e7a2f24b
Matthew Jaikaran <mattjaikaran@gmail.com>
2018-03-09 19:01:55 -0500

    Users can see results

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,9 @@ import reduxThunk from 'redux-thunk'
 
 import App from './components/App'
 import reducers from './reducers'
-
+// dev only below
+import axios from 'axios'
+window.axios = axios
 
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
